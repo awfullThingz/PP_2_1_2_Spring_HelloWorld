1ec6480e3a3eca18d0d4ab8e19628100bd6acc0f
awfullThingz <116511523+awfullThingz@users.noreply.github.com>
2023-01-19 23:29:15 +0700

    commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,14 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat catBean1 =
+                (Cat) applicationContext.getBean("cat");
+        Cat catBean2 =
+                (Cat) applicationContext.getBean("cat");
         System.out.println(bean.getMessage());
+        System.out.println("helloworld1 = helloworld2 - " + (bean == bean2));
+        System.out.println("cat1 = cat2 - " + (catBean1 == catBean2));
     }
 }
